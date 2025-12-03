a6d53f23b49f609a37aa7447e810ba3366b37f02
Arisnt01 <aguilarcaceresabelkevin@gmail.com>
2025-12-03 12:20:53 -0500

    aaaaa!

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api.vocatio.com',
+  apiUrl: 'https://vocatio-api-latest.onrender.com/api/v1',
   //apiKey: 'your-api-key'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: true,
-  apiUrl: 'https://vocatio-api-latest.onrender.com',
+  production: false,
+  apiUrl: 'http://localhost:8080/api/v1',
   //apiKey: 'your-api-key'
 };
