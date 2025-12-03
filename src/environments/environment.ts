f74c4d04e17fe1462caf6b75b2b49510b96c0fcd
Arisnt01 <aguilarcaceresabelkevin@gmail.com>
2025-12-03 10:41:00 -0500

    cambiando a produccion

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:8080/api/v1',
+  production: true,
+  apiUrl: 'https://vocatio-api-latest.onrender.com/api/v1',
   //apiKey: 'your-api-key'
 };
