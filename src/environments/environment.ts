33f40ab41c6b76b59f1e3923c4f3c4f9b52913a4
Arisnt01 <aguilarcaceresabelkevin@gmail.com>
2025-12-03 11:40:16 -0500

    correccion en la dirección del render

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://vocatio-api-latest.onrender.com/api/v1',
+  apiUrl: 'https://vocatio-api-latest.onrender.com',
   //apiKey: 'your-api-key'
 };
